3abfed5922aa35944d8f8f7fe0e3f141b73c71b9
dav1jim <davidricardojimenezlopez@gmail.com>
2022-08-29 15:01:26 -0600

    tipos de variable

---
+++ src/main/java/com/mycompany/tiposdevariable/Tiposdevariable.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tiposdevariable;
+
+/**
+ *
+ * @author david
+ */
+
+// variables java
+public class Tiposdevariable {
+    public static void main(String[] args) {
+// declarar la variable spped de tipo int (numeros enteros)
+
+int spped=20;
+//actualizar el contenido de la variable spped:
+//si ya habias declarado la variable
+//declarar la variable y asignarle un valor al mismo tiempo
+int salary=100;
+//crear una variable de tipo String:
+String eployeename= "David jimenez";
+
+    }
+}
